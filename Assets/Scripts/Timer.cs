5927e50ca42995e829e58da8eedbce005844985e
Abdullah Azzam Azhar <abdullahazzam98@mail.ugm.ac.id>
2019-12-15 19:10:00 +0700

    first commit

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [Tooltip("Berapa Lama delay")]
+    public float delay;
+
+    private float timer;
+
+    public UnityEvent onDuar;
+    
+    private bool timerActive;
+
+    public void StartTimer()
+    {
+        timerActive = true;
+        timer = delay;
+    }
+    
+
+    private void FixedUpdate()
+    {
+        if (timerActive)
+        {
+            
+            timer = timer - Time.deltaTime;
+            if (timer <=0)
+            {
+                onDuar.Invoke();
+                print("duuarr");
+                timerActive = false;
+            }
+        }
+    }
+}
